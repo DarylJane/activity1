cd38bba297ddacbd937da1a3fc81bb31f4e88d7d
DarylJane <52590267+DarylJane@users.noreply.github.com>
2019-07-13 14:19:15 +0800

    Add files via upload

--- model/usermodel.php
+++ model/usermodel.php
@@ -12,22 +12,39 @@ class UserModel{
         $this->db = new database();
     }
     public function insert(){
-        $sql = "INSERT INTO $this->tablename (name, email,address,contact) VALUES('$this->name','$this->email','$this->address', '$this->contact')";
-        $this->db->execute($sql);
+        $sql = "INSERT INTO $this->tablename (name,contact) VALUES('$this->name','$this->contact')";
+         if ( $this->db->execute($sql) == 1){
+        return true;
+       }else{
+           return false;
+       }
     }
-    public function update(){
-        $sql = "UPDATE INTO $this->tablename (name,email,address,contact) SET('$this->name') WHERE('$this->id)";
-        $this->db->execute($sql);
-    }
+    public function update($id){
+        $sql= "UPDATE $this->tablename SET contact='$this->contact' WHERE id=$id";
+
+        if ( $this->db->execute($sql) == 1){
+            return true;
+        }else{
+            return false;
+        }
+   }
+    
 
     public function readAll(){
         $sql = "SELECT * FROM $this->tablename";
         return $this->db->execute($sql);
     }
 
-    public function delete(){
-        $sql = "DELETE FROM $this->tablename (name,email,address,contact) WHERE('$this->id)";
-    }
+    public function delete($id){
+        $sql = "DELETE FROM $this->tablename WHERE id =$id";
+        if ($this->db->execute($sql) == 1) {
+            return true;
+        }
+            else{
+                return false;
+            }
+        }
+    
     public function __destruct(){
         unset($this->db);
     }
